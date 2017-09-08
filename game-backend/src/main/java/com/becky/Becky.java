62219016030be83820b77a32a8c687bddf9d6eee
Clayton Hunsinger <chunsinger@bridge2solutions.com>
2017-09-08 08:50:23 -0400

    Java - Initial Server Design

---
+++ game-backend/src/main/java/com/becky/Becky.java
@@ -0,0 +1,11 @@
+package com.becky;
+
+/**
+ * The main application class of the game.
+ * Created by chunsinger on 9/8/2017.
+ */
+public class Becky {
+    public static void main(final String... args) {
+
+    }
+}
