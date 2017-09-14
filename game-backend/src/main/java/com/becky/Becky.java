2d8e5f59b545cb353afe486db5907164cd0f9ed2
wgparish <williamparish159@gmail.com>
2017-09-14 15:56:58 -0400

    This is an updated version of the initialize webSocket

--- game-backend/src/main/java/com/becky/Becky.java
+++ game-backend/src/main/java/com/becky/Becky.java
@@ -1,7 +1,6 @@
 package com.becky;
 
-import java.net.ServerSocket;
-import java.net.Socket;
+import java.net.InetSocketAddress;
 
 /**
  * The main application class of the game.
@@ -9,30 +8,9 @@ import java.net.Socket;
  */
 public class Becky {
     public static void main(final String[] args) {
-        try {
-            //Creates the Server socket on port 9001
-            ServerSocket server;
-            try {
-                server = new ServerSocket(9001);
+        InetSocketAddress socketAddress = new InetSocketAddress(9001);
+        SimpleServer simpleServer = new SimpleServer(socketAddress);
 
-                System.out.println("Server has started. Port is " + server.getLocalPort() +
-                        ". Waiting for a connection");
-            }catch (Exception e){
-                System.out.println(e.toString());
-            }
-
-            //Listens for a client
-            try {
-                Socket client = server.accept();
-
-                System.out.println("A client has connected: \n" +
-                    client.toString());
-            }catch (Exception e){
-                System.out.println(e.toString());
-            }
-
-        }catch (Exception e){
-            System.out.println(e.toString());
-        }
+        simpleServer.start();
     }
 }
