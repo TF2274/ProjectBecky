3a8f3d50a850aea5bd5167acb1a60028e11d06c2
Clayton Hunsinger <clayton30022@hotmail.com>
2017-09-25 19:24:38 -0400

    Client-side initial player status object

---
+++ game-frontend/src/main/ts/networked/InitialServerJoinState.ts
@@ -0,0 +1,10 @@
+import {Point} from "../Point";
+/**
+ * This contains the initial state of a player upon joining the server.
+ * This includes the initially assigned username, authentication string, and position
+ */
+export class InitialServerJoinState {
+    public initialUsername: string;
+    public authenticationString: string;
+    public initialLocation: Point;
+}
